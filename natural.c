79a3eb093a754be89c5fa456d1fa11c9c6139f0c
Aastik Biswas <aastikbiswas2007@gmail.com>
2025-10-21 13:53:37 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main () {
+    int n;
+    int i;
+    printf("Enter the value of n :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++) {
+     printf("\n%d",i);
+    }
+    return 0;
+}
